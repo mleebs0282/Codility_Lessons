dee100666196bfb07cdc10e6c2a8792a1349ec8c
mleebs0282 <43883374+mleebs0282@users.noreply.github.com>
2020-04-26 14:07:48 -0400

    Added the majority of lesson 7

---
+++ 7_StacksAndQueues/Brackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int solution(string S)
+            {
+                if (S.Length % 2 != 0) return 0;
+
+                Stack stack = new Stack();
+
+                for (int i = 0; i < S.Length; i++)
+                {
+                    var current = S[i].ToString();
+                    if (current == "[" || current == "(" || current == "{") {
+                        stack.Push(current);
+                    } else {
+                        if (stack.Count == 0)
+                        {
+                            return 0;
+                        }
+
+                        var removed = stack.Pop().ToString();
+
+                        if (removed == "[" && current == "]"
+                         || removed == "(" && current == ")"
+                         || removed == "{" && current == "}")
+                        {
+                            continue;
+                        }
+                        else return 0;
+                    }
+                }
+                if (stack.Count == 0) return 1;
+
+                return 0;
+            }
+
+            Console.WriteLine(solution("{[()()]}"));
+            //Console.WriteLine(solution("({{({}[]{})}}[]{})"));
+            //Console.WriteLine(solution("()(()()(((()())(()()))"));
+        }
+    }
+}
---
+++ 7_StacksAndQueues/Fish/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int solution(int[] A, int[] B)
+            {
+                int count = 0;
+                int liveFish = 0;
+                var stack = new Stack<int>();
+
+                for (int i = 0; i < A.Length; i++) {
+                    if (B[i] == 0) {
+                        while (stack.Count > 0 && A[i] > stack.Peek())
+                            stack.Pop();
+
+                        if (stack.Count == 0)
+                            count++;
+                    } else if (B[i] == 1)
+                        stack.Push(A[i]);
+                }
+                liveFish = stack.Count + count;
+
+                return liveFish;
+            }
+            var fishSize = new int[] { 4, 3, 2, 1, 5 };
+            var stream = new int[] { 0, 1, 0, 0, 0 };
+            Console.WriteLine(solution(fishSize, stream));
+        }
+    }
+}
